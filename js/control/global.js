7117a85ed757fb0ac64c85e78dca3f1611137c86
Vítor Castelo Branco <vtcb@cin.ufpe.br>
2016-03-03 17:18:46 -0300

    Add Creature object
    Implemets Player inheritance from Creature.

--- js/control/global.js
+++ js/control/global.js
@@ -4,5 +4,8 @@ var Global = {
     	radius          : 1,
     	acceleration    : 0.5,
     	deceleration    : 0.1
+    },
+    nullV3 : function() {
+        return new THREE.Vector3(0, 0, 0);
     }
 };
--- js/game/player.js
+++ js/game/player.js
@@ -1,20 +1,12 @@
 var Player = function(kbh, radius, position) {
+	var obj      = Creature.call(this);
+
 	var kbh      = kbh;
 	var geometry = new THREE.SphereGeometry(radius, 50, 50);
 	var texture  = undefined;
 	var material = new THREE.MeshNormalMaterial();
 	var mesh     = new THREE.Mesh(geometry, material);
 
-	var nullV3 = function() {
-		return new THREE.Vector3(0, 0, 0);
-	};
-
-	var acceleration = nullV3();
-
-	var speed = nullV3();
-
-	var acceleration_module = Global.player.acceleration;
-	var deceleration_module = Global.player.deceleration;
 	var position = position;
 
 	var acceleratingDirections = [];
@@ -53,7 +45,7 @@ var Player = function(kbh, radius, position) {
 			}
 		);
 
-		acceleration = nullV3();
+		var acceleration = Global.nullV3();
 
 		for(var i in acceleratingDirections) {
 			var direction = acceleratingDirections[i];
@@ -61,65 +53,16 @@ var Player = function(kbh, radius, position) {
 		}
 
 		acceleration.normalize();
-		acceleration.multiplyScalar(acceleration_module);
+		acceleration.multiplyScalar(obj.getAccMod());
+
+		obj.setAcc(acceleration);
 	};
 
-	var accelerate = function() {
-		speed.add(acceleration);
-	};
+	obj.setUpdateAcceleration(updateAcceleration);
+	obj.setAccMod(Global.player.acceleration);
+	obj.setDecMod(Global.player.deceleration);
+	obj.setPos(position);
+	obj.setMesh(mesh);
 
-	var decelerate = function() {
-		if(deceleration_module >= speed.length()) {
-			speed = nullV3();
-		} else {
-			var deceleration = speed.clone();
-			deceleration.normalize();
-			deceleration.multiplyScalar(-deceleration_module);
-			speed.add(deceleration);
-		}
-	};
-
-	var move = function() {
-		accelerate();
-		position.add(speed);
-		decelerate();
-
-		/* Check collision */
-	};
-
-	var updateMesh = function() {
-		mesh.position.x = position.x;
-		mesh.position.y = position.y;
-		mesh.position.z = position.z;
-	};
-
-	return {
-		getMesh 	: function()	{
-			return mesh;
-		},
-		setMesh 	: function(mesh)	{
-			this.mesh = mesh;
-		},
-		getPosition : function()	{
-			return position;
-		},
-		setPosition : function(pos)	{
-			this.position = pos;
-		},
-		getSpeed    : function()	{
-			return speed;
-		},
-		setSpeed 	: function(speed)	{
-			this.speed = speed;
-		},
-		getPositionStr : function()	{
-			return '' + [position.x, position.y, position.z];
-		},
-
-		update 		: function() {
-			updateAcceleration();
-			move();
-			updateMesh();
-		}
-	};
+	return obj;
 };
